4dbd67e4231278f158a1e3848bb765c96c1449ef
ICCG <110618005+ItsCommandCraftGaming@users.noreply.github.com>
2026-06-30 21:07:23 +0300

    am actualizat designul si am inserat quick view

--- frontend/src/components/CarItem/CarItem.tsx
+++ frontend/src/components/CarItem/CarItem.tsx
@@ -3,12 +3,14 @@ import "./CarItem.css"
 import { useFavorites } from "../../hooks/useFavorites"
 import { IMG_BASE_URL } from "../../data/constants"
 import { useBasket } from "../../hooks/useBasket"
-
+import { useState } from "react"
+import { createPortal } from "react-dom"
 type Props = {
     car: Car
 }
 
 export function CarItem({ car }: Props) {
+    const [isModalOpen, setIsModalOpen] = useState(false)
     const equipments = car.equipment.split(",")
     const { toggleFavorite, isFavorite } = useFavorites()
     const { addToBasket, removeFromBasket, isItemInBasket } = useBasket()
@@ -24,25 +26,12 @@ export function CarItem({ car }: Props) {
                 <div className="row"><div className="label">Model: </div>{car.model}</div>
                 <div className="row"><div className="label">Construction Year: </div>{car.constructionYear}</div>
                 <div className="row"><div className="label">Fuel type: </div>{car.fuelType}</div>
-                <div className="row"><div className="label">Mileage: </div>{car.mileage} km</div>
-                <div className="row"><div className="label">Engine size: </div>{car.engineSize} cm3</div>
-                <div className="row"><div className="label">Power: </div>{car.power} CP</div>
-                <br />
-                <div className="row">
-                    <div className="label">
-                        Equipments:
-                    </div>
-                </div>
-                <div className="row">
-                    <ul className="list">
-                        {equipments.slice(0, 9).map((equipment, index) => {
-                            return <li key={index}>{equipment}</li>
-                        })}
-                    </ul>
-                </div>
             </div>
             <div className="price">Price: {car.price} EUR</div>
             <div className="row">
+                <button className="button button--quick-view" onClick={() => setIsModalOpen(true)}>
+                    Quick View
+                </button>
                 <button className="button" onClick={() => toggleFavorite(car)}>
                     {isFavorite(car) ? "Remove from favorites" : "Add to favorites"}
                 </button>
@@ -56,6 +45,40 @@ export function CarItem({ car }: Props) {
                     </button>
                 )}
             </div>
+
+            {isModalOpen && createPortal(
+                <div className="modal-overlay" onClick={() => setIsModalOpen(false)}>
+                    <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+                        <button className="modal-close" onClick={() => setIsModalOpen(false)}>&times;</button>
+                        <h2 className="modal-title">{car.manufacturer} {car.model}</h2>
+                        <div className="modal-body">
+                            <div className="modal-image-container">
+                                <img src={`${IMG_BASE_URL}/${car.image}`} className="modal-car-image" />
+                            </div>
+                            <div className="modal-info">
+                                <div className="row"><div className="label">Construction Year: </div>{car.constructionYear}</div>
+                                <div className="row"><div className="label">Fuel type: </div>{car.fuelType}</div>
+                                <div className="row"><div className="label">Mileage: </div>{car.mileage} km</div>
+                                <div className="row"><div className="label">Engine size: </div>{car.engineSize} cm3</div>
+                                <div className="row"><div className="label">Power: </div>{car.power} CP</div>
+                                <div className="row"><div className="label">Price: </div><div className="price-tag">{car.price} EUR</div></div>
+                                <br />
+                                <div className="row">
+                                    <div className="label">Equipments:</div>
+                                </div>
+                                <div className="row">
+                                    <ul className="list full-list">
+                                        {equipments.map((equipment, index) => {
+                                            return <li key={index}>{equipment}</li>
+                                        })}
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>,
+                document.body
+            )}
         </div>
     )
 }
